3673fb57d64e1756985813a233819b6a0a8875ce
Ace <f.a.lucifer@gmail.com>
2017-02-28 21:34:40 +0800

    before convert to 2015

--- DataAnalysisTool/Test.Designer.cs
+++ DataAnalysisTool/Test.Designer.cs
@@ -28,37 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lbxProduct = new System.Windows.Forms.ListBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.lbxRev = new System.Windows.Forms.ListBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.gbc = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lblinfo = new System.Windows.Forms.Label();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbxUL = new System.Windows.Forms.TextBox();
+            this.tbxLL = new System.Windows.Forms.TextBox();
+            this.tbxParameter = new System.Windows.Forms.TextBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.label4 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.listBox3 = new System.Windows.Forms.ListBox();
-            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.lbxParameter = new System.Windows.Forms.ListBox();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.groupBox5.SuspendLayout();
             this.groupBox4.SuspendLayout();
             this.groupBox3.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.lbxProduct);
             this.groupBox1.Location = new System.Drawing.Point(12, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(200, 238);
@@ -66,24 +66,19 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Product";
             // 
-            // listBox1
+            // lbxProduct
             // 
-            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Items.AddRange(new object[] {
-            "1",
-            "2",
-            "3",
-            "4"});
-            this.listBox1.Location = new System.Drawing.Point(3, 16);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(194, 219);
-            this.listBox1.TabIndex = 0;
-            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.lbxProduct.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbxProduct.FormattingEnabled = true;
+            this.lbxProduct.Location = new System.Drawing.Point(3, 16);
+            this.lbxProduct.Name = "lbxProduct";
+            this.lbxProduct.Size = new System.Drawing.Size(194, 219);
+            this.lbxProduct.TabIndex = 0;
+            this.lbxProduct.SelectedIndexChanged += new System.EventHandler(this.lbxProduct_SelectedIndexChanged);
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.listBox2);
+            this.groupBox2.Controls.Add(this.lbxRev);
             this.groupBox2.Location = new System.Drawing.Point(215, 12);
             this.groupBox2.Name = "groupBox2";
             this.groupBox2.Size = new System.Drawing.Size(200, 238);
@@ -91,34 +86,71 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Program Rev";
             // 
-            // listBox2
+            // lbxRev
             // 
-            this.listBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listBox2.FormattingEnabled = true;
-            this.listBox2.Location = new System.Drawing.Point(3, 16);
-            this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(194, 219);
-            this.listBox2.TabIndex = 0;
+            this.lbxRev.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbxRev.FormattingEnabled = true;
+            this.lbxRev.Location = new System.Drawing.Point(3, 16);
+            this.lbxRev.Name = "lbxRev";
+            this.lbxRev.Size = new System.Drawing.Size(194, 219);
+            this.lbxRev.TabIndex = 0;
+            this.lbxRev.SelectedIndexChanged += new System.EventHandler(this.lbxRev_SelectedIndexChanged);
             // 
             // panel1
             // 
             this.panel1.AutoScroll = true;
             this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.gbc);
+            this.panel1.Controls.Add(this.groupBox5);
+            this.panel1.Controls.Add(this.lblinfo);
             this.panel1.Controls.Add(this.groupBox4);
             this.panel1.Controls.Add(this.groupBox2);
             this.panel1.Controls.Add(this.groupBox3);
-            this.panel1.Controls.Add(this.chart1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(950, 593);
+            this.panel1.Size = new System.Drawing.Size(950, 603);
             this.panel1.TabIndex = 7;
+            // 
+            // gbc
+            // 
+            this.gbc.Location = new System.Drawing.Point(12, 294);
+            this.gbc.Name = "gbc";
+            this.gbc.Size = new System.Drawing.Size(908, 297);
+            this.gbc.TabIndex = 6;
+            this.gbc.TabStop = false;
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.listBox1);
+            this.groupBox5.Location = new System.Drawing.Point(710, 317);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(200, 238);
+            this.groupBox5.TabIndex = 5;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "Test Item";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(3, 16);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(194, 219);
+            this.listBox1.TabIndex = 0;
+            // 
+            // lblinfo
+            // 
+            this.lblinfo.Location = new System.Drawing.Point(15, 254);
+            this.lblinfo.Name = "lblinfo";
+            this.lblinfo.Size = new System.Drawing.Size(911, 26);
+            this.lblinfo.TabIndex = 4;
             // 
             // groupBox4
             // 
-            this.groupBox4.Controls.Add(this.textBox3);
-            this.groupBox4.Controls.Add(this.textBox2);
-            this.groupBox4.Controls.Add(this.textBox1);
+            this.groupBox4.Controls.Add(this.tbxUL);
+            this.groupBox4.Controls.Add(this.tbxLL);
+            this.groupBox4.Controls.Add(this.tbxParameter);
             this.groupBox4.Controls.Add(this.checkBox1);
             this.groupBox4.Controls.Add(this.label4);
             this.groupBox4.Controls.Add(this.label3);
@@ -131,26 +163,26 @@
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "Criteria";
             // 
-            // textBox3
+            // tbxUL
             // 
-            this.textBox3.Location = new System.Drawing.Point(60, 138);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(89, 20);
-            this.textBox3.TabIndex = 7;
+            this.tbxUL.Location = new System.Drawing.Point(60, 138);
+            this.tbxUL.Name = "tbxUL";
+            this.tbxUL.Size = new System.Drawing.Size(89, 20);
+            this.tbxUL.TabIndex = 7;
             // 
-            // textBox2
+            // tbxLL
             // 
-            this.textBox2.Location = new System.Drawing.Point(60, 102);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(89, 20);
-            this.textBox2.TabIndex = 6;
+            this.tbxLL.Location = new System.Drawing.Point(60, 102);
+            this.tbxLL.Name = "tbxLL";
+            this.tbxLL.Size = new System.Drawing.Size(89, 20);
+            this.tbxLL.TabIndex = 6;
             // 
-            // textBox1
+            // tbxParameter
             // 
-            this.textBox1.Location = new System.Drawing.Point(63, 41);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(185, 20);
-            this.textBox1.TabIndex = 5;
+            this.tbxParameter.Location = new System.Drawing.Point(63, 41);
+            this.tbxParameter.Name = "tbxParameter";
+            this.tbxParameter.Size = new System.Drawing.Size(185, 20);
+            this.tbxParameter.TabIndex = 5;
             // 
             // checkBox1
             // 
@@ -199,7 +231,7 @@
             // 
             // groupBox3
             // 
-            this.groupBox3.Controls.Add(this.listBox3);
+            this.groupBox3.Controls.Add(this.lbxParameter);
             this.groupBox3.Location = new System.Drawing.Point(421, 12);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Size = new System.Drawing.Size(200, 238);
@@ -207,51 +239,33 @@
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Test Item";
             // 
-            // listBox3
+            // lbxParameter
             // 
-            this.listBox3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listBox3.FormattingEnabled = true;
-            this.listBox3.Location = new System.Drawing.Point(3, 16);
-            this.listBox3.Name = "listBox3";
-            this.listBox3.Size = new System.Drawing.Size(194, 219);
-            this.listBox3.TabIndex = 0;
-            // 
-            // chart1
-            // 
-            chartArea1.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(12, 283);
-            this.chart1.Name = "chart1";
-            series1.ChartArea = "ChartArea1";
-            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series1.IsXValueIndexed = true;
-            series1.Legend = "Legend1";
-            series1.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Square;
-            series1.Name = "Series1";
-            series1.SmartLabelStyle.IsMarkerOverlappingAllowed = true;
-            this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(911, 300);
-            this.chart1.TabIndex = 0;
-            this.chart1.Text = "chart1";
+            this.lbxParameter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lbxParameter.FormattingEnabled = true;
+            this.lbxParameter.Location = new System.Drawing.Point(3, 16);
+            this.lbxParameter.Name = "lbxParameter";
+            this.lbxParameter.Size = new System.Drawing.Size(194, 219);
+            this.lbxParameter.TabIndex = 0;
+            this.lbxParameter.SelectedIndexChanged += new System.EventHandler(this.lbxParameter_SelectedIndexChanged);
             // 
             // frmTest
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(950, 593);
+            this.ClientSize = new System.Drawing.Size(950, 603);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.panel1);
             this.Name = "frmTest";
             this.Text = "Test";
+            this.Load += new System.EventHandler(this.frmTest_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox2.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
+            this.groupBox5.ResumeLayout(false);
             this.groupBox4.ResumeLayout(false);
             this.groupBox4.PerformLayout();
             this.groupBox3.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -260,21 +274,24 @@
         #endregion
 
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox lbxProduct;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox lbxRev;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.Windows.Forms.GroupBox groupBox4;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbxUL;
+        private System.Windows.Forms.TextBox tbxLL;
+        private System.Windows.Forms.TextBox tbxParameter;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.ListBox lbxParameter;
+        private System.Windows.Forms.Label lblinfo;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox gbc;
     }
 }
--- DataAnalysisTool/Test.cs
+++ DataAnalysisTool/Test.cs
@@ -7,19 +7,221 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Windows.Forms.DataVisualization.Charting;
+using MySql.Data.MySqlClient;
 
 namespace DataAnalysisTool
 {
     public partial class frmTest : Form
     {
+        string cs = @"server=192.168.21.52;userid=webuser;password=Vanchip301;database=testdata";
+        //string cs = @"server=45.76.104.155;userid=webuser;password=Vanchip301;database=testdata";
+
         public frmTest()
         {
             InitializeComponent();
         }
 
-        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void frmTest_Load(object sender, EventArgs e)
         {
+            MySqlConnection conn = null;
+            try
+            {
+                conn = new MySqlConnection(cs);
+                conn.Open();
+                lblinfo.Text = conn.ServerVersion.ToString();
+                
+                string strcmd = "SELECT DISTINCT Product FROM sessioninfo";
+                using (MySqlCommand sqlcmd = new MySqlCommand(strcmd, conn))
+                {
+                    //MySqlDataAdapter da = new MySqlDataAdapter(strcmd, conn);
+                    //DataSet ds = new DataSet();
+                    //da.Fill(ds, "Authors");
+                    //DataTable dt = ds.Tables["Authors"];
+                    //dt.WriteXml("authors.xml");
 
+                    lbxProduct.Items.Clear();
+                    MySqlDataReader dr = sqlcmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        lbxProduct.Items.Add(dr.GetString(0));
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                lblinfo.Text = ex.ToString();
+            }
+            finally
+            {
+                if (conn != null)  conn.Close();
+            }
+        }
+
+        private void lbxProduct_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MySqlConnection conn = null;
+            try
+            {
+                conn = new MySqlConnection(cs);
+                conn.Open();
+                lblinfo.Text = conn.ServerVersion.ToString();
+
+                string strcmd = "SELECT DISTINCT ProgramRev FROM sessioninfo WHERE Product LIKE @Product";
+                using (MySqlCommand sqlcmd = new MySqlCommand(strcmd, conn))
+                {
+                    lbxRev.Items.Clear();
+                    sqlcmd.Parameters.Add(new MySqlParameter("@Product", lbxProduct.SelectedItem.ToString()));
+                    MySqlDataReader dr = sqlcmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        lbxRev.Items.Add(dr.GetString(0));
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                lblinfo.Text = ex.ToString();
+            }
+            finally
+            {
+                if (conn != null) conn.Close();
+            }
+        }
+        private void lbxRev_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MySqlConnection conn = null;
+            try
+            {
+                conn = new MySqlConnection(cs);
+                conn.Open();
+                lblinfo.Text = conn.ServerVersion.ToString();
+                
+                string strcmd = "SELECT Parameter FROM testcpk WHERE SessionID IN "
+                    +"(SELECT SessionID FROM sessioninfo WHERE Product LIKE @Product AND ProgramRev LIKE @Rev) "
+                    +"GROUP BY Parameter;";                
+                using (MySqlCommand sqlcmd = new MySqlCommand(strcmd, conn))
+                {
+                    lbxParameter.Items.Clear();
+                    sqlcmd.Parameters.Add(new MySqlParameter("Product", lbxProduct.SelectedItem.ToString()));
+                    sqlcmd.Parameters.Add(new MySqlParameter("Rev", lbxRev.SelectedItem.ToString()));
+                    MySqlDataReader dr = sqlcmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        lbxParameter.Items.Add(dr.GetString(0));
+                    }
+                }
+            }
+            catch (MySqlException ex)
+            {
+                lblinfo.Text = ex.ToString();
+            }
+            finally
+            {
+                if (conn != null) conn.Close();
+            }
+        }
+        private void lbxParameter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            MySqlConnection conn = null;
+
+            List<string> xValues = new List<string>();
+            List<double> yValues = new List<double>();
+            double dblAverage;
+            double dblStdev;
+
+
+            try
+            {
+                conn = new MySqlConnection(cs);
+                conn.Open();
+                lblinfo.Text = conn.ServerVersion.ToString();
+
+                string strcmd = "SELECT SessionID, Average FROM testcpk WHERE SessionID IN "
+                    +"(SELECT SessionID FROM sessioninfo WHERE Product LIKE @Product AND ProgramRev LIKE @Rev AND TestQuantity > @Quantity) "
+                    +"AND Parameter LIKE @Parameter";
+                     
+                using (MySqlCommand sqlcmd = new MySqlCommand(strcmd, conn))
+                {
+                    gbc.Controls.Clear();
+                    listBox1.Items.Clear();
+
+                    sqlcmd.Parameters.Add(new MySqlParameter("@Product", lbxProduct.SelectedItem.ToString()));
+                    sqlcmd.Parameters.Add(new MySqlParameter("@Rev", lbxRev.SelectedItem.ToString()));
+                    sqlcmd.Parameters.Add(new MySqlParameter("@Quantity", 1000));
+                    sqlcmd.Parameters.Add(new MySqlParameter("@Parameter", lbxParameter.SelectedItem.ToString()));
+
+                    MySqlDataReader dr = sqlcmd.ExecuteReader();
+
+                    while (dr.Read())
+                    {
+                        xValues.Add(dr.GetString(0));
+                        yValues.Add(Math.Round(dr.GetDouble(1), 3));
+                    }
+                    dblAverage = yValues.Average();
+                    dblStdev = calc_stdev(yValues);
+
+                    tbxParameter.Text = dblAverage + "," + dblStdev.ToString();
+
+                    Chart chart = new Chart();
+
+                    Series series_data = new Series("Default");
+                    series_data.ChartType = SeriesChartType.FastPoint;
+                    chart.Series.Add(series_data);
+
+                    Series series_ll = new Series("LL");
+                    series_ll.ChartType = SeriesChartType.FastLine;
+                    chart.Series.Add(series_ll);
+
+                    Series series_ul = new Series("UL");
+                    series_ul.ChartType = SeriesChartType.FastLine;
+                    chart.Series.Add(series_ul);
+
+
+                    ChartArea chartArea = new ChartArea();
+                    Axis yAxis = new Axis(chartArea, AxisName.Y);
+                    Axis xAxis = new Axis(chartArea, AxisName.X);
+                    
+
+                    //chart.Series["Default"].Points.DataBindXY(xValues, yValues);
+                    for (int i = 0; i < yValues.Count; i++)
+                    {
+                        chart.Series["Default"].Points.AddY(yValues[i]);
+                        chart.Series["LL"].Points.AddY(dblAverage - 3 * dblStdev);
+                        chart.Series["UL"].Points.AddY(dblAverage + 3 * dblStdev);
+                    }
+                    //while (dr.Read())
+                    //{
+                    //    //chart.Series["Default"].Points.AddY(dr.GetDouble(1));
+                    //}
+
+                    chart.ChartAreas.Add(chartArea);
+                    chart.Dock = DockStyle.Fill;
+                    gbc.Controls.Add(chart);                    
+                }
+            }
+            catch (MySqlException ex)
+            {
+                lblinfo.Text = ex.ToString();
+            }
+            finally
+            {
+                if (conn != null) conn.Close();
+            }
+        }
+
+        private double calc_stdev( List<double> values)
+        {
+            double stdev = 0;
+            double vsum = 0;
+            List<double> variance = new List<double>();
+
+            for (int i = 0; i < values.Count; i++)
+            {
+                //variance.Add(Math.Pow((values.Average() - values[i]), 2));
+                vsum += Math.Pow((values.Average() - values[i]), 2);
+            }
+            stdev = Math.Sqrt(vsum / values.Count);
+            return stdev;
         }
     }
 }
